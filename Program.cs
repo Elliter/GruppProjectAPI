41433becc20998f86f23441dd94e2be54ad087be
Martins <martingaluzinskis2112@gmail.com>
2025-11-17 14:41:22 +0100

    Removed unnecessary files/folders when creating the project.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,2 @@
 using GruppProjectAPI.Business;
 using GruppProjectAPI.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddRazorPages();
-builder.Services.AddHttpClient<IWeatherService, WeatherService>();
-builder.Services.AddScoped<WeatherManager>();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-
-app.UseStaticFiles();
-app.UseRouting();
-app.MapRazorPages();
-app.Run();

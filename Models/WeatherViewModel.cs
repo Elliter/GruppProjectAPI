06d08152f1b2f88f7ca063df7514b919b30e91bf
Malleck Navid <Malleck Navid@DESKTOP-KDAQT06>
2025-11-19 00:06:23 +0100

    weather view added

---
+++ Models/WeatherViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GruppProjectAPI.Models
+{
+    public class ResultViewModel
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Description { get; set; }
+        public string Humidity { get; set; }
+        public string TempFeelsLike { get; set; }
+        public string Temp{ get; set; }
+        public string TempMax { get; set; }
+        public string TempMin { get; set; }
+        public string WeatherIcon { get; set; }
+    }
+
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public int temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+
+  
+}

8d8c12185a8aeb65b34f899daa33a721439965b3
Martins <martingaluzinskis2112@gmail.com>
2025-11-19 00:21:37 +0100

    Added some backend, to test if it will get some information on html.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,27 +1,21 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using GruppProjectAPI.Business;
-using GruppProjectAPI.Moduler;
+﻿using GruppProjectAPI.Business;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GruppProjectAPI.Business;
 
 namespace GruppProjectAPI.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly WeatherManager _manager;
+        private readonly WeatherAggregator _agg;
+        public double AvgTemp { get; set; }
+        public double AvgRain { get; set; }
+        public bool Failed { get; set; }
 
-        public List<WeatherResponse> WeatherList { get; set; }
-        public double AverageTemp { get; set; }
-
-        public IndexModel(WeatherManager manager)
-        {
-            _manager = manager;
-        }
+        public IndexModel(WeatherAggregator agg) { _agg = agg; }
 
         public async Task OnGet()
         {
-            var cities = new List<string> { "Göteborg", "Borås" };
-            (WeatherList, AverageTemp) = await _manager.GetMultipleWeathersAsync(cities);
+
         }
     }
 }
--- Services/WeatherServiceAPI.cs
+++ Services/WeatherServiceAPI.cs
@@ -1,6 +1,31 @@
-﻿namespace GruppProjectAPI.Services
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using GruppProjectAPI.Models;
+
+namespace GruppProjectAPI.Services
 {
-    public class WeatherServiceAPI
+    public class WeatherService
     {
+        private readonly HttpClient _http;
+        private const double Latitude = 57.72;
+        private const double Longitude = 12.94;
+        private readonly string _location;
+
+        public WeatherService(HttpClient http, IConfiguration config)
+        {
+            var end = DateTime.UtcNow.Date;
+            var start = end.AddDays(-7);
+            string s = start.ToString("yyyy-MM-dd");
+            string e = end.ToString("yyyy-MM-dd");
+
+            _http = http;
+            _location = config.GetValue<string>("WeatherStack:Location") ?? "Boras";
+
+            var url = $"https://archive-api.open-meteo.com/v1/archive?latitude={Latitude}&longitude={Longitude}&start_date={s}&end_date={e}&hourly=temperature_2m,precipitation&timezone=Europe%2FStockholm";
+        }
     }
 }
